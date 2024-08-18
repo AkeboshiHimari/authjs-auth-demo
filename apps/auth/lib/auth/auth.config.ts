1c3f1077bb9af1c94ca3aac3dfbbcfff7e79d75c
AkeboshiHimari <156805377+AkeboshiHimari@users.noreply.github.com>
2024-08-18 20:39:45 +0900

    chore: Remove cookies setting in auth.config.ts

--- apps/auth/lib/auth/auth.config.ts
+++ apps/auth/lib/auth/auth.config.ts
@@ -1,9 +1,6 @@
 import Twitter from "next-auth/providers/twitter";
 import Google from "next-auth/providers/google";
-
 import type { NextAuthConfig } from "next-auth";
-const isProduction = process.env.NODE_ENV === "production";
-
 export default {
 	providers: [
 		Google({
@@ -21,18 +18,4 @@ export default {
 			},
 		}),
 	],
-	cookies: {
-		sessionToken: {
-			name: `${isProduction ? "__Secure-" : ""}authjs.session-token`,
-			options: {
-				httpOnly: true,
-				sameSite: "lax",
-				path: "/",
-				secure: isProduction,
-				domain: isProduction
-					? `.${process.env.NEXT_PUBLIC_BASE_URL}`
-					: "localhost",
-			},
-		},
-	},
 } satisfies NextAuthConfig;
